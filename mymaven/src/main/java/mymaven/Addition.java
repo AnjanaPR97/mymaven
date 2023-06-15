e579596bb56fc55538bcf2299e89bc3356b54d86
user <user@DESKTOP-UMO1HLL>
2023-06-15 22:44:46 +0530

    First commit

---
+++ mymaven/src/main/java/mymaven/Addition.java
@@ -0,0 +1,16 @@
+package mymaven;
+
+public class Addition
+{
+
+	public static void main(String args[])
+	{
+	int a=11;
+	int b=12;
+	int c= a+b;
+	System.out.println(c);
+	}
+	
+	
+	
+}
---
+++ mymaven/src/main/java/mymaven/Simple.java
@@ -0,0 +1,14 @@
+package mymaven;
+
+public class Simple 
+{
+	
+		public static void main(String args[])
+		{
+		System.out.println("Hello World");
+		}
+	
+	
+	
+	
+}
